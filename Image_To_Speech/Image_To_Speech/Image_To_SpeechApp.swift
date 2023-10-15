0b5fa085d041e9c708cccd4e8f1d25203640ba18
SahajPatel17 <147888259+SahajPatel17@users.noreply.github.com>
2023-10-15 15:04:31 -0400

    Add files via upload

---
+++ Image_To_Speech/Image_To_Speech/Image_To_SpeechApp.swift
@@ -0,0 +1,17 @@
+//
+//  Image_To_SpeechApp.swift
+//  Image_To_Speech
+//
+//  Created by bca on 10/15/23.
+//
+
+import SwiftUI
+
+@main
+struct Image_To_SpeechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
