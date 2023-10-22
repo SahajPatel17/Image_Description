1a9b20c94df464572c085dc247f5173fb8a7eff2
SahajPatel17 <147888259+SahajPatel17@users.noreply.github.com>
2023-10-22 11:22:17 -0400

    Reverted test edits

--- Image_To_Speech/Image_To_Speech/ContentView.swift
+++ Image_To_Speech/Image_To_Speech/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {easy
+        VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
